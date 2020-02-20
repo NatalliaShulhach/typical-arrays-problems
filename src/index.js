228f5562dab772938d7312a2a67a862eb5db5e25
KitarBit <nataredred@gmail.com>
2020-02-20 10:03:43 +0300

    mod: modified index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=0) {
+  if(array == false){
+    return 0;
+  }
+  for (i = 0; i < array.length; i++){
+    if(array[i] < array[0]){
+      array[0] = array[i];
+    }
+  }
+  return array[0]
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=0) {
+  if(array == false){
+    return 0;
+  }
+  
+  for (i = 0; i < array.length; i++){
+    if(array[i] > array[0]){
+      array[0] = array[i];
+    }
+  }
+  return array[0]
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg (array=0) {
+  if(array == false){
+    return 0;
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
+
